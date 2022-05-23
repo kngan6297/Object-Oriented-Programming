201a6641435d71e30ae06c90ab49c2356ea1b31c
Ngân <kngan6297@gmail.com>
2022-05-23 16:15:30 +0700

    first commit

---
+++ Bai1_ToaDo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_ToaDo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ToaDo toaDo1 = new ToaDo(1,1, "A");
+            Console.WriteLine(toaDo1.toString());
+        }
+    }
+}
---
+++ Bai1_ToaDo/ToaDo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_ToaDo
+{
+    public class ToaDo
+    {
+        private int x;
+        private int y;
+        private string tenToaDo;
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public string getTenToaDo()
+        {
+            return tenToaDo;
+        }
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+        public void setTenToaDo(string tenToaDo)
+        {
+            this.tenToaDo = tenToaDo;
+        }
+        public String toString()
+        {
+            return this.tenToaDo + "(" + this.x + "," + this.y + ")";
+        }
+        public ToaDo()
+        {
+        }
+        public ToaDo(int x, int y, string tenToaDo)
+        {
+            this.x = x;
+            this.y = y;
+            this.tenToaDo = tenToaDo;
+        }
+    }
+
+   
+}
+ 
