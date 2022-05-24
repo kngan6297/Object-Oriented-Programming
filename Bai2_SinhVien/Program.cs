aac7ce0b30af899f7e53f06e8c83fee4f2fa8aa4
Ngân <kngan6297@gmail.com>
2022-05-24 10:12:49 +0700

    first commit

---
+++ Bai2_SinhVien/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_SinhVien
+{
+    public class SinhVienMain
+    {
+        static void Main(String[] args)
+        {
+            SinhVien sv1 = new SinhVien(11111, "Ly Thi Kim Ngan", (float)8.5, (float)9.5);
+            SinhVien sv2 = new SinhVien(22222, "Tran Ba Quan Thuy", 7, 8);
+            SinhVien sv3 = new SinhVien();
+            int maSV = 0;
+            while (maSV <= 0)
+            {
+                Console.WriteLine("Nhap ma so cua sinh vien sv3:");
+                maSV = Convert.ToInt32(Console.ReadLine());
+                if (maSV <= 0)
+                {
+                    Console.WriteLine("Ma so cua sinh vien khong hop le");
+                }
+            }
+            sv3.setMaSV(maSV);
+            string name = "";
+            while (name.Length == 0)
+            {
+                Console.WriteLine("Nhap ho va ten cua sinh vien sv3:");
+                name = Console.ReadLine();
+                if (name.Length == 0)
+                {
+                    Console.WriteLine("Ho va ten cua sinh vien khong hop le");
+                }
+            }
+            sv3.setHoTen(name);
+            float diemLT = -1;
+            while (diemLT < 0 || diemLT > 10)
+            {
+                Console.WriteLine("Nhap diem ly thuyet cua sinh vien sv3:");
+                diemLT = float.Parse(Console.ReadLine());
+                if (diemLT < 0 || diemLT > 10)
+                {
+                    Console.WriteLine("Diem ly thuyet cua sinh vien khong hop le");
+                }
+            }
+            sv3.setDiemLT(diemLT);
+            float diemTH = -1;
+            while (diemTH < 0 || diemTH > 10)
+            {
+                Console.WriteLine("Nhap diem thuc hanh cua sinh vien sv3:");
+                diemTH = float.Parse(Console.ReadLine());
+                if (diemTH < 0 || diemTH > 10)
+                {
+                    Console.WriteLine("Diem thuc hanh cua sinh vien khong hop le");
+                }
+            }
+            sv3.setDiemTH(diemTH);
+            Console.WriteLine("\n{0,-10} {1,-30} {2,-10} {3,-10} {4,-10}", "MaSV", "HoTen", "DiemLT", "DiemTH", "DiemTB");
+            sv1.hienthiSV();
+            sv2.hienthiSV();
+            sv3.hienthiSV();
+        }
+    }
+}
---
+++ Bai2_SinhVien/SinhVien.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_SinhVien
+{
+    public class SinhVien
+    {
+        private int MaSV;
+        private string HoTen;
+        private float DiemLT;
+        private float DiemTH;
+        //constructor mặc định
+        public SinhVien()
+        {
+        }
+        //constructor đầy đủ
+        public SinhVien(int MaSV, string HoTen, float DiemLT, float DiemTH)
+        {
+            this.MaSV = MaSV;
+            this.HoTen = HoTen;
+            this.DiemLT = DiemLT;
+            this.DiemTH = DiemTH;
+        }
+        public int getMaSV()
+        {
+            return MaSV;
+        }
+        public void setMaSV(int MaSV)
+        {
+            this.MaSV = MaSV;
+        }
+        public String getHoTen()
+        {
+            return HoTen;
+        }
+        public void setHoTen(String HoTen)
+        {
+            this.HoTen = HoTen;
+        }
+        public float getDiemLT()
+        {
+            return DiemLT;
+        }
+        public void setDiemLT(float DiemLT)
+        {
+            if (DiemLT < 0 || DiemLT > 10)
+            {
+                this.DiemLT = 0;
+            }
+            else
+            {
+                this.DiemLT = DiemLT;
+            }
+        }
+        public float getDiemTH()
+        {
+            return DiemTH;
+        }
+        public void setDiemTH(float DiemTH)
+        {
+            if (DiemTH < 0 || DiemTH > 10)
+            {
+                this.DiemTH = 0;
+            }
+            else
+            {
+                this.DiemTH = DiemTH;
+            }
+        }
+        public float tinhDiemTB()
+        {
+            return (DiemLT + DiemTH) / 2;
+        }
+        public void hienthiSV()
+        {
+            Console.WriteLine(this.toString());
+        }
+        public String toString()
+        {
+            return String.Format("{0,-10} {1,-30} {2,-10} {3,-10} {4,-10}", this.MaSV, this.HoTen, this.DiemLT, this.DiemTH, this.tinhDiemTB());
+
+        }
+    }
+}
