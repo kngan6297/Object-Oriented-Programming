4df74bae7af9d0305dc0e476ea5dceff3dfc4252
Ngân <kngan6297@gmail.com>
2022-05-26 11:27:06 +0700

    Bai 4

---
+++ Bai4_Vehicle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4_Vehicle
+{
+    public class VehicleMain
+    {
+        static void Main(String[] args)
+        {
+            Vehicle xe1 = new Vehicle("Nguyen Thu Loan", "Future Neo", 100, 35000000);
+            Vehicle xe2 = new Vehicle("Le Minh Tinh", "Ford Ranger", 3000, 250000000);
+            Vehicle xe3 = new Vehicle("Nguyen Minh Triet", "Landscape", 1500, 1000000000);
+            Console.WriteLine("{0,-20} {1,-20} {2,10} {3,20} {4,20}", "Ten chu xe", "Loai xe", "Dung tich", "Tri gia", "Thue phai nop");
+            Console.WriteLine("==============================================================================================");
+            xe1.HienthiXe();
+            xe2.HienthiXe();
+            xe3.HienthiXe();
+        }
+    }
+}
---
+++ Bai4_Vehicle/Vehicle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4_Vehicle
+{
+    public class Vehicle
+    {
+        private string TenChuXe;
+        private string LoaiXe;
+        private double DungTich;
+        private double TriGia;
+        private double ThuePhaiNop;
+        //constructor mặc định
+        public Vehicle()
+        {
+
+        }
+        //constructor đủ tham số
+        public Vehicle(string TenChuXe, string LoaiXe, float DungTich, float TriGia)
+        {
+            this.TenChuXe = TenChuXe;
+            this.LoaiXe = LoaiXe;
+            this.DungTich = DungTich;
+            this.TriGia = TriGia;
+        }
+        //getter
+        public string getTenChuXe()
+        {
+            return TenChuXe;
+        }
+        public string getLoaiXe()
+        {
+            return LoaiXe;
+        }
+        public double getDungTich()
+        {
+            return DungTich;
+        }
+        public double getTriGia()
+        {
+            return TriGia;
+        }
+        //setter
+        public void setTenChuXe(string TenChuXe)
+        {
+            this.TenChuXe = TenChuXe;
+        }
+        public void setLoaiXe(string LoaiXe)
+        {
+            this.LoaiXe = LoaiXe;
+        }
+        public void setDungTich(int DungTich)
+        {
+            this.DungTich = DungTich;
+        }
+        public void setTriGia(int TriGia)
+        {
+            this.TriGia = TriGia;
+        }
+        public double tinhThuePhaiNop()
+        {
+            if(DungTich < 100)
+            {
+                this.ThuePhaiNop = 0.01 * this.TriGia;
+            } else if(DungTich >= 100 && DungTich < 200)
+            {
+                this.ThuePhaiNop = 0.03 * this.TriGia;
+            }
+            else
+            {
+                this.ThuePhaiNop = 0.05 * this.TriGia;
+            }
+            return this.ThuePhaiNop;
+        }
+        public void HienthiXe()
+        {
+            Console.WriteLine(this.toString());
+        }
+        public string toString()
+        {
+            return String.Format("{0,-20} {1,-20} {2,10} {3,20:#,##0.00} {4,20:#,##0.00}", this.TenChuXe, this.LoaiXe, this.DungTich, this.TriGia, this.tinhThuePhaiNop());
+        }
+    }
+}
